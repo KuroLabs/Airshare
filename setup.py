a7346c7d4c3b982230aedbb5a318ab0b5947abdd
nandahkrishna <nanda.harishankar@gmail.com>
2020-04-22 05:12:15 +0530

    Bumped to version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="Airshare",
-    version="0.0.1",
+    version="0.0.2",
     author="Kandavel A, Mohanasundar M, Nanda H Krishna",
     author_email="kurolabs.org+airshare@gmail.com",
     description="An easy way to share content in a local network.",
