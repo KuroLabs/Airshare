52550736bf0cc1d2a59106c3a9708d6c9a63aba9
nandahkrishna <nanda.harishankar@gmail.com>
2020-05-16 14:59:16 +0530

    Fixed content-disposition bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Kandavel A, Mohanasundar M, Nanda H Krishna'
 author = 'Kandavel A, Mohanasundar M, Nanda H Krishna'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
